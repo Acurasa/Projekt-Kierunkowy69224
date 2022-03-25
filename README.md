80540e66d34f81b3f7396d636cb9c607d58e6e65
Leonid Stasyuk <unxetra@gmail.com>
2022-03-25 22:02:36 +0100

    v.1.3

---
+++ README.md
@@ -0,0 +1,1 @@
+# Projekt-Kierunkowy69224
--- interface/styling.py
+++ interface/styling.py
@@ -2,5 +2,5 @@ BG_COLOR = "thistle2"
 BG_COLOR_2 = "plum3"
 FG_COLOR = "gray12"
 FG_COLOR_2 = "magenta4"
-GLOBAL_FONT = ("Calibri", 11, "normal")
-BOLD_FONT = ("Calibri", 11, "bold")
+GLOBAL_FONT = ("Times New Roman", 14, "bold")
+BOLD_FONT = ("Times New Roman", 14, "bold")
